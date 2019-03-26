b3b65ba200e1229d9808f08a84d9ede60f5e36d1
Stefan Zukin <stefan.zukin@gmail.com>
2019-03-25 23:19:23 -0700

    started update to use model.star files

--- classReport.py
+++ classReport.py
@@ -2,7 +2,9 @@
 # coding: utf-8
 
 import glob
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def parseStar(starfile, keep_index=False, augment=False):
     headers = []
@@ -37,21 +39,40 @@ with open(runJob, "r") as f:
         if l.startswith("Number of classes"):
             numClasses = (int)(l.split("== ")[1])
 
+#Iterate through model.star files, reading the class distribution
+modelStars = glob.glob('*model.star')
+it = 1
+classDict = {}
+for name in modelStars:
+    print("Reading " + name)
+
 #Read the data.star files, iterating through them
-dataStars = glob.glob('*data.star')
-for name in dataStars:
-    dataFilename = name
-    df = parseStar(dataFilename)
-    classes = {}
-    for column in df:
-        if column == "rlnClassNumber":
-            for i in df[column]:
-                if i not in classes :
-                    classes[i] = 1
-                else:
-                    classes[i] = classes[i] + 1
-    print(classes)
-    if micrographs == 0:
-        for i in classes:
-            micrographs += classes[i]
-print(micrographs)
+# dataStars = glob.glob('*data.star')
+# it = 1
+# classDict = {}
+# for name in dataStars:
+#     print("Reading " + name)
+#     dataFilename = name
+#     df = parseStar(dataFilename)
+#     classes = []
+#     for i in range(numClasses):
+#         classes.append(0)
+#     for column in df:
+#         if column == "rlnClassNumber":
+#             for i in df[column]:
+#                 classes[i-1] = classes[i-1] + 1
+#     print(classes)
+#     if micrographs == 0:
+#         for i in classes:
+#             micrographs += i
+#     classDict["it_" + str(it)] = classes
+#     it += 1
+# cd = pd.DataFrame.from_dict(classDict, orient='index')
+# cd.plot()
+# plt.show()
+
+
+
+#TODO: This whole thing is more than I had to do. I don't actually have to read in all the data and then iterate through it, the 
+#Class distribution is stored in the model.star file, so I just have to read that in. I'll keep this code because it might be useful
+#But that will be a much faster way to do this.
