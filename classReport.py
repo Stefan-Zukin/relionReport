3be1bd9ea84a1e7c0dd676e0de61844de2ffa6b7
Stefan Zukin <stefan.zukin@gmail.com>
2019-03-26 00:25:52 -0700

    Got a working graph

--- classReport.py
+++ classReport.py
@@ -5,29 +5,72 @@ import glob
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sb
 
-def parseStar(starfile, keep_index=False, augment=False):
+#ParesStar function taken from PyEM
+def parseStar(starFile, tableName, keep_index=False, augment=False):
     headers = []
+    foundTable = False
     foundheader = False
     ln = 0
-    with open(starfile, "r") as f:
+    lb = 0
+    i = 0
+    with open(starFile, "r") as f:
         for l in f:
-            if l.startswith("_"):
-                foundheader = True
-                lastheader = True
-                if keep_index:
-                    head = l.rstrip()
-                else:
-                    head = l.split('#')[0].rstrip().lstrip('_')
-                headers.append(head)
+            if(l.startswith(tableName)):
+                foundTable = True
             else:
-                lastheader = False
-            if foundheader and not lastheader:
-                break
-            ln += 1
-    df = pd.read_csv(starfile, skiprows=ln, delimiter='\s+', header=None)
+                ln += 1
+                if foundheader and not lastheader:
+                    #Don't add to skipped line value if I'm reading the data
+                    ln -= 1
+            if(foundTable):
+                if l.startswith("_"):
+                    foundheader = True
+                    lastheader = True
+                    if keep_index:
+                        head = l.rstrip()
+                    else:
+                        head = l.split('#')[0].rstrip().lstrip('_')
+                    headers.append(head)
+                else:
+                    lastheader = False
+                if foundheader and not lastheader:
+                    #Read through the data to see how long it is, record length in lb
+                    #Use this value in the pd.read_csv line
+                    if l.startswith(" "):
+                        break
+                    lb += 1
+    # print("ln:" + str(ln))
+    # print("lb:" + str(lb))
+    df = pd.read_csv(starFile, skiprows=ln, delimiter='\s+', nrows=lb, header=None)
     df.columns = headers
     return df  #A PANDAS data frame object is returned
+
+
+#     #Original ParesStar function taken from PyEM
+# def parseStar(starfile, keep_index=False, augment=False):
+#     headers = []
+#     foundheader = False
+#     ln = 0
+#     with open(starfile, "r") as f:
+#         for l in f:
+#             if l.startswith("_"):
+#                 foundheader = True
+#                 lastheader = True
+#                 if keep_index:
+#                     head = l.rstrip()
+#                 else:
+#                     head = l.split('#')[0].rstrip().lstrip('_')
+#                 headers.append(head)
+#             else:
+#                 lastheader = False
+#             if foundheader and not lastheader:
+#                 break
+#             ln += 1
+#     df = pd.read_csv(starfile, skiprows=ln, delimiter='\s+', header=None)
+#     df.columns = headers
+#     return df  #A PANDAS data frame object is returned
         
 
 #Use the runJob file to determine the number of classes without having to iterate through the whole data.star file        
@@ -38,13 +81,34 @@ with open(runJob, "r") as f:
     for l in f:
         if l.startswith("Number of classes"):
             numClasses = (int)(l.split("== ")[1])
-
+    
 #Iterate through model.star files, reading the class distribution
-modelStars = glob.glob('*model.star')
-it = 1
+modelStars = sorted(glob.glob('*model.star'))
 classDict = {}
+it = 0
 for name in modelStars:
-    print("Reading " + name)
+    # print("Reading " + name)
+    filename = name
+    df = parseStar(filename, "data_model_classes")
+    classDist = []
+    for column in df:
+        if column == "rlnClassDistribution":
+            classDist = list(df[column])
+    classDict[it] = classDist
+    it += 1
+    # print(df)
+cd = pd.DataFrame.from_dict(classDict, orient='index')
+headers = []
+legend = ''
+for i in range(numClasses):
+    headers.append("class_" + str(i))
+    legend += str(i+1)
+cd.columns = headers
+print(cd)
+
+cd.plot(use_index=True)
+plt.legend(legend, ncol=2, loc='upper left');
+plt.show()
 
 #Read the data.star files, iterating through them
 # dataStars = glob.glob('*data.star')
