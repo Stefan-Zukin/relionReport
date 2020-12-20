85d8fde410ab4e4b2af9e70a115d58815d2a5ed1
Stefan Zukin <stefan.zukin@gmail.com>
2020-12-19 19:37:09 -0800

    Updated base functionality for Relion 3.1 and ChimeraX

--- relionReport.py
+++ relionReport.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# coding: utf-8
+#coding: utf-8
 
 import glob
 import argparse
@@ -14,11 +14,13 @@ from datetime import datetime
 try:
     import pandas as pd
     from IPython.display import display, HTML
-    from chimera import runCommand as rc
-    from chimera import replyobj
 except:
     pass
 
+CHIMERA_PATH = "/Applications/ChimeraX-1.1.1.app/Contents/MacOS/ChimeraX"
+LEVEL = 6
+
+#"Example: relion_reportX % python relionReportX.py -v "lighting flat" -s "supersample 2 width 100 height 100 transparentBackground true" -m Class3D/job029"
 
 class starTable():
 
@@ -122,9 +124,12 @@ class relionJob():
         s = model.split("run_")[1]
         s = s.split("it")
         it = int(s[1][0:3])
-        if s[0].startswith("run_ct"):
+        if 'ct' in s[0]:
             it += 1
         return it
+
+    def __getCurrentDirectory(self):
+        return os.getcwd()
 
     def __readModelStars(self):
         modelStars = glob.glob(self.path + '/run_*t*model.star')
@@ -162,42 +167,35 @@ class relionJob():
 
     def renderMovie(self):
         """Sets up the script to be run from chimera so it will render a movie."""
-        chimera = ""  # Enter your chimera executable location here
-        if sys.platform == "linux" or sys.platform == "linux2":
-            # linux
-            chimera = "/programs/x86_64-linux/chimera/1.13.1/bin/chimera"
-        elif sys.platform == "darwin":
-            # OS X
-            chimera = "/Applications/Chimera.app/Contents/MacOS/chimera"
-        else:
-            if(chimera == ""):
-                raise Exception("Your operating system was not recognized so the script cannot auto-detect your chimera executable location. "
-                                + "Please manually edit the script to include your chimera executable location at line 190")
+        print("Connecting to ChimeraX")
+        chimera = CHIMERA_PATH  # Enter your chimera executable location here
+        # linux
+        #chimera = "/programs/x86_64-linux/chimera/1.13.1/bin/chimera"
+        # OS X
+        #chimera = "/Applications/Chimera.app/Contents/MacOS/chimera"
+        if(chimera == ""):
+            raise Exception("Your operating system was not recognized so the script cannot auto-detect your chimera executable location. "
+                            + "Please manually edit the script to include your chimera executable location at line 165")
         selfPath = os.path.realpath(__file__)
-        argsString = " "
-        if(args.f):
-            argsString += "-f "
-        if(args.r):
-            argsString += "-r "
-        if(args.hr):
-            argsString += "-hr "
-        imagePath = self.path + "Images"
+        argsString = "-v '{}' ".format(args.v[0]) + "-s '{}' ".format(args.s[0])
+        imagePath = self.__getCurrentDirectory() + "/" + self.jobName + "Images"
+        print("Rendering")
+        command = chimera + " --script " + "\"" + selfPath + " -type " + self.jobType() + " " + argsString + self.path + "\""
+        # subprocess.run('echo {}'.format(command))
         subprocess.run(chimera + " --script " + "\"" +
-                       selfPath + " -chimera -type " + self.jobType() + " " + argsString + self.path + "\"", shell=True)
+                       selfPath + " -type " + self.jobType() + " " + argsString + self.path + "\"", shell=True)
         os.chdir(imagePath)
-        print(os.getcwd())
         try:
             subprocess.call(
                 "ffmpeg -r 10 -f image2 -s 1920x1080 -i frame%04d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p movie.mp4", shell=True)
         except:
-            print(
-                "Rendering frames into movie failed. Check that you have ffmpeg installed")
+            print("Rendering frames into movie failed. Check that you have ffmpeg installed")
             print("Try manually running the command from the chimeraImages folder: ffmpeg -r 10 -f image2 -s 1920x1080 -i it%04d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p movie.mp4")
 
     def graphToPDF(self):
         """Save the graphs to a pdf with the job name"""
         pp = PdfPages(self.jobName + '.pdf')
-        style = args.s[0]
+        style = args.style[0]
         print("Using style " + style)
         print("Saving graphs as " + self.jobName + ".pdf")
         for t in self.tables:
@@ -235,7 +233,7 @@ class class3D(relionJob):
         self.parameters.append("rlnClassDistribution")
         self.parameters.append("rlnEstimatedResolution")
         self.parameters.append("rlnAccuracyRotations")
-        self.parameters.append("rlnAccuracyTranslations")
+        self.parameters.append("rlnAccuracyTranslationsAngst")
 
     def format(self, p):
         """Graph formatting rules for class3D"""
@@ -249,6 +247,32 @@ class class3D(relionJob):
         self.graphToPDF()
 
 
+class initialModel(relionJob):
+
+    def __addParameters(self):
+        """Defines the parameters to be graphed"""
+        self.parameters.append("rlnCurrentResolution")
+        self.parameters.append("rlnCurrentImageSize")
+        self.parameters.append("rlnSigmaOffsetsAngst")
+        self.parameters.append("rlnLogLikelihood")
+        self.parameters.append("rlnAveragePmax")
+
+    def format(self, p):
+        """Graph formatting rules for class3D"""
+        p.xlabel("Iteration")
+        ax = p.gca()
+        ax.get_legend().remove()
+
+    def format(self, p):
+        p.xlabel("Iteration/10")
+
+    def __init__(self, path):
+        super(initialModel, self).__init__(path)
+        self.__addParameters()
+        self.read("data_model_general")
+        self.graphToPDF()
+
+
 class refine3D(relionJob):
 
     def __readModelStars(self):
@@ -257,8 +281,6 @@ class refine3D(relionJob):
         if len(modelStars) == 0:
             raise Exception("ERROR: Could not find a model.star file")
         modelStars.sort(key=super(refine3D, self).sortModelStars)
-        for name in modelStars:
-            print(name)
         return modelStars
 
     def format(self, p):
@@ -271,7 +293,7 @@ class refine3D(relionJob):
         """Defines the parameters to be graphed"""
         self.parameters.append("rlnEstimatedResolution")
         self.parameters.append("rlnAccuracyRotations")
-        self.parameters.append("rlnAccuracyTranslations")
+        self.parameters.append("rlnAccuracyTranslationsAngst")
         self.parameters.append("rlnOverallFourierCompleteness")
 
     def __init__(self, path):
@@ -294,7 +316,7 @@ class chimeraRenderer():
     def __sortMrcs(self, model=" "):
         """Sorting method to sort by mrc iteration number"""
 
-        s = model.split("it")
+        s = model.split("_it")
         it = int(s[1][0:3])
         if s[0].startswith("run_ct"):
             it += 1
@@ -311,25 +333,28 @@ class chimeraRenderer():
         """Goes to the directory given in path, then reads in all the .mrc files
         Iterates over the read MRCs, organizing them by iteration number and class number.
         Returns a dictionary where the keys are the iterations in order, and the item is a list
-        with the classes for that iteration in order """
+        with the paths to the classes for that iteration in order """
 
         mrcs = glob.glob(self.path + '/*.mrc')
         if(self.jobType == "Refine3D"):
             mrcs = glob.glob(self.path + '/*half1_class001.mrc')
+        if(self.jobType == 'InitialModel'):
+            mrcs = [x for x in mrcs if 'class' in x]
         if len(mrcs) == 0:
             raise Exception("ERROR: Could not find any mrc files")
         mrcs.sort(key=self.__sortMrcs)
         iterations = {}
         for fn in mrcs:
-            s = fn.split("it")
+            s = fn.split("_it")
             it = int(s[1][0:3])
-            if s[0].startswith("run_ct"):
+            if self.jobType == "InitialModel":
+                it = int(it / 10)
+            if "ct" in fn:
                 it += 1
             if(it in iterations):
                 iterations[it] = iterations[it] + [fn]
             else:
                 iterations[it] = [fn]
-
         # If we're rendering a refinement, add the maximixed version as the final iteration
         if(self.jobType == "Refine3D"):
             iterations[it+1] = glob.glob(self.path + '/run_class001.mrc')
@@ -338,93 +363,57 @@ class chimeraRenderer():
             iterations[k].sort(key=self.__sortClasses)
         return iterations
 
-    def __saveImage(self, png_name, raytrace, flat, highRes, closeModelsAfterSaving=True, autoFitWindow=True):
+    def __saveImage(self, png_name, visuals, save, closeModelsAfterSaving=True, autoFitWindow=True):
         """Save an image from chimera into the output folder"""
-        try:
-            from chimera import runCommand as rc
-        except:
-            pass
-        rc("lighting contrast .7")
-        rc("lighting sharpness 100")
-        rc("lighting reflectivity .8")
-        rc("lighting brightness 1.1")
-        raytraceString = ""
-        if(raytrace):
-            rc("lighting reflectivity .8")
-            rc("lighting brightness 0.85")
-            raytraceString = "raytrace rtwait rtclean "
-        resolutionString = ""
-        if(highRes):
-            resolutionString = "width 8  height 8 dpi 256 units inches"
-        else:
-            resolutionString = "width 8  height 8 dpi 128 units inches"
-        if autoFitWindow:
-            rc("windowsize 1024 1024")
-            rc("window")
-        if(flat):
-            rc("lighting mode ambient")
-            rc("set silhouetteWidth 16")
-        else:
-            rc("lighting mode two-point")
-            rc("set silhouetteWidth 4")
-        rc("copy file " + png_name +
-           " supersample 4 " + raytraceString + resolutionString)
+        run(session, visuals)
+        run(session, "save " + png_name + " " + save)
         if closeModelsAfterSaving:
-            rc("close all")
+            run(session, "close all")
 
     def render(self):
         """Render frames of the iterations in chimera"""
-        try:
-            from chimera import runCommand as rc
-        except:
-            pass
-        for it in self.iterations.keys():
-            modelNum = 0
-            rc("cd " + self.path)
+        for it in list(self.iterations.keys()):
+            run(session, "cd " + self.path)
+            model_number = 1
             for c in self.iterations[it]:
-                rc("open " + c)
-                rc("volume #" + str(modelNum) + " sdlevel 8")
-                if(self.jobType == "Refine3D"):
-                    rc("volume #" + str(modelNum) + " sdlevel 10")
-                modelNum += 1
-            rc("volume all step 1")
-            rc("tile")
-            rc("preset apply publication 1")
-            num = str(it)
-            while len(num) < 4:
-                num = "0" + num
-            png_name = "frame" + num + ".png"
-            rc("cd " + self.output)
-            self.__saveImage(png_name, args.r, args.f, args.hr)
+                run(session, "open " + c)
+                run(session, "volume #{} sdlevel {}".format(model_number, LEVEL))
+                model_number += 1
+                
+            run(session, "volume all step 1")
+            run(session, "tile")
+            run(session, "preset publication")
+            run(session, "view")
+            png_name = "frame" + f'{it:04}' + ".png"
+            run(session, "cd " + self.output)
+            self.__saveImage(png_name, visuals=self.args.v[0], save=self.args.s[0])
             subprocess.call("echo \"Finished iteration " + str(it) + " at " +
                             datetime.now().strftime('%H:%M:%S') + "\"", shell=True)
             finalIt = it
 
-        # Spin with the final iteration
-        modelNum = 0
+        #Spin with the final iteration
         subprocess.call("echo \"Rendering final spin at " +
                         datetime.now().strftime('%H:%M:%S') + "\"", shell=True)
-        rc("cd " + path)
+        run(session, "cd " + self.path)
+        model_number = 1
         for c in self.iterations[it]:
-            rc("open " + c)
-            rc("volume #" + str(modelNum) + " sdlevel 8")
-            if(self.jobType == "Refine3D"):
-                rc("volume #" + str(modelNum) + " sdlevel 10")
-            modelNum += 1
-        rc("volume all step 1")
-        rc("tile")
-        rc("preset apply publication 1")
-        rc("window")
+            run(session, "open " + c)
+            run(session, "volume #{} sdlevel {}".format(model_number, LEVEL))
+            model_number += 1
+
+        run(session, "volume all step 1")
+        run(session, "tile")
+        run(session, "preset publication")
         for x in range(90):
-            rc("turn y 4 model #0-" + str(modelNum))
-            num = str(finalIt + 1 + x)
-            while len(num) < 4:
-                num = "0" + num
-            png_name = "frame" + num + ".png"
-            rc("cd " + self.output)
-            self.__saveImage(png_name, args.r, args.f, args.hr,
-                             False, False)
-        rc("stop now")
+            model_number = 1
+            for c in self.iterations[it]:
+                run(session, "turn y 4 model #{} center #{}".format(model_number, model_number))
+                model_number += 1
+            num = finalIt + 1 + x
+            png_name = "frame" + f'{num:04}' + ".png"
+            run(session, "cd " + self.output)
+            self.__saveImage(png_name, visuals=self.args.v[0], save=self.args.s[0], closeModelsAfterSaving=False)
+        run(session, "exit")
 
     def makeOutputFolder(self):
         """Create the jobImages output folder"""
@@ -437,14 +426,13 @@ class chimeraRenderer():
             pass
 
     def __init__(self, path, args):
-        self.path = path
+        self.path = path  #Path to the job folder
         self.jobType = args.type[0]
-        subprocess.call("echo \"Recognized by Chimera as " +
-                        self.jobType + "\"", shell=True)
+        self.args = args
         self.makeOutputFolder()
         self.iterations = self.__readMrcs()
         self.render()
-        self.args = args
+        
 
 
 def parseArgs():
@@ -455,48 +443,53 @@ def parseArgs():
     parser.add_argument(
         '-i', dest='i', action='store_true', help='Show the interactive form of the graphs in addition to saving to a PDF.')
     parser.add_argument(
-        '-m', dest='m', action='store_true', help='Render a movie of the job using Chimera')
+        '-m', dest='m', action='store_true', help='Render a movie of the job using ChimeraX')
     parser.add_argument(
-        '-r', dest='r', action='store_true', help='Render images using Chimera raytracing')
+        '-v', nargs=1, default=["lighting soft"], help='Specify visual settings to be used during ChimeraX rendering.')
     parser.add_argument(
-        '-f', dest='f', action='store_true', help='Render images as flat, without shadows or highlights')
+        '-s', nargs=1, default=["supersample 4 width 1024 height 1024 transparentBackground true"], help='Specify save settings to be used during ChimeraX rendering.')
     parser.add_argument(
-        '-hr', dest='hr', action='store_true', help='Render higher resolution images. Will make the process slower.')
-    parser.add_argument(
-        '-s', nargs=1, default=["seaborn-paper"], help='Use this argument to define which style matplotlib will use to plot the graphs.')
-    parser.add_argument(
-        '-chimera', dest='chimera', action='store_true', help='Execute the script run from chimera, should not be used by any user')
+        '-style', nargs=1, default=["seaborn-paper"], help='Use this argument to define which style matplotlib will use to plot the graphs.')
     parser.add_argument(
         '-type', nargs=1, help='Input the job type, should not be used by any user')
     args = parser.parse_args()
     return args
 
 
+if "ChimeraX" in __name__:
+    from chimerax.core.commands import run
+    args = parseArgs()
+    path = args.path[0]
+    chimeraRenderer(path, args)
+
+
 if __name__ == '__main__':
     args = parseArgs()
     path = args.path[0]
-
+    
     """ If this script is called from chimera, then args.chimera = True
     If this is the case, we run a totally different script than if it were
     called by the used alone"""
     job = relionJob(path)
-    if(args.chimera):
-        renderer = chimeraRenderer(path, args)
-    else:
-        print("Recognized job as " + job.jobType())
-        if(job.jobType() == "Class3D"):
-            job = class3D(path)
-        elif(job.jobType() == "Refine3D"):
-            job = refine3D(path)
-
-        if(args.i):
-            job.graph()
-        if(args.m):
-            job.renderMovie()
+    print("Recognized job as " + job.jobType())
+    if(job.jobType() == "Class3D"):
+        job = class3D(path)
+    elif(job.jobType() == "Refine3D"):
+        job = refine3D(path)
+    elif(job.jobType() == "InitialModel"):
+        job = initialModel(path)
+    if(args.i):
+        job.graph()
+    if(args.m):
+        job.renderMovie()
 
 """
 TODO:
--Make it not crash when filling the table with N/A values, such as during a alignment free classification
+-Make it not crash when filling the table with N/A values, such as during a alignment free classification (This seems to be fine...)
 -Make it work with ChimeraX
 -Make sure that the Refine3D videos are representative. Try with a more dramatic refinement, i couldn't really see any improvement in the one I did.
+-If a job is continued twice, ie. ct_20 & ct_30, the ct_30 will have double the data because 
+ it will already increment by one for ct_20 and won't know to increment by 2 for ct_30
+ Maybe make it able to set the SD level manually, currently would have to edit the script
+    Maybe make global variablse at the top that can be easily edited.
 """
